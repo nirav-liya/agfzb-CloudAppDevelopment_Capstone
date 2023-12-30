99796a20fb4100eb656625b966a8107159c3247b
nirav-liya <nirav.liya@gmail.com>
2023-12-30 02:24:28 -0500

    update

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     # path for about view
     path('about/', view=views.about, name='about'),
     # path for contact us view
-
+     path('contact/', view=views.contact, name='contact'),
     # path for registration
 
     # path for login
