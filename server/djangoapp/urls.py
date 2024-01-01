a6292569f995c48ad4815af9e30faefe6d137b61
nirav-liya <nirav.liya@gmail.com>
2024-01-01 02:42:08 -0500

    updated

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     # path for dealer reviews view
     path('dealer/<int:dealer_id>/', views.get_dealer_details, name='dealer_details'),
     # path for add a review view
+    path(route='dealer/<int:dealer_id>/add-review/', view=views.add_review, name="add_review")
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
